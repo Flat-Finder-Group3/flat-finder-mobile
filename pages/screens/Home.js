2b66bd81e532f0117e5a8b45e52eaba07f4ddbf4
giabertu <giabertu99@gmail.com>
2023-03-30 18:31:42 +0100

    feat: added two card to style

--- pages/screens/Home.js
+++ pages/screens/Home.js
@@ -1,23 +1,29 @@
 import {CommonActions} from '@react-navigation/native'
 import React, { useState } from 'react';
 import { useEffect } from 'react';
-import { View, StyleSheet, Button, StatusBar, FlatList } from 'react-native';
+import { View, StyleSheet, Button, StatusBar, FlatList, RefreshControl, ScrollView} from 'react-native';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Text, BottomNavigation } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { styles } from '../../styles'
 import TicketCard from '../../components/TicketCard';
+import FavListingCard from '../../components/FavListingCard';
 
-export default function Home({navigation, route, tickets, loading}) {
+
+export default function Home({navigation, route, tickets, ownListings, favListings, loading, fetchData}) {
+
+  favListings = favListings.map((item) => item.listing);
 
 
-  // const { tickets, ownListings, favListings, loading} = route.params
-  // const [homeTickets, setHomeTickets] = useState(tickets)
+  const [refreshing, setRefreshing] = useState(false);
 
-  // useEffect(() => {
-    // console.log("Tickets from HOME: ", homeTickets)
-  // }, [homeTickets])
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await fetchData();
+    setRefreshing(false);
+  };
+
 
   if (loading) return (
     <View>
@@ -26,14 +32,37 @@ export default function Home({navigation, route, tickets, loading}) {
   )
 
   return (
-    <View style={styles.container}>
+    <ScrollView
+    contentContainerStyle={styles.container}
+    refreshControl={
+      <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+    }
+  >
+
+    {/* <View style={styles.container}> */}
         <Text>My empty home!</Text>
-          <Text>My tickets!</Text>
+
+          <Text>Favorite Listings</Text>
+            <FlatList
+              data={favListings}
+              renderItem={({item}) => <FavListingCard key={item.id} item={item}/> }
+              keyExtractor={(item) => item.id.toString()}
+              />
+
+
+          <Text>Own Listings</Text>
+            <FlatList
+              data={ownListings}
+              renderItem={({item}) => <FavListingCard key={item.id} item={item}/> }
+              keyExtractor={(item) => item.id.toString()}
+              />
+
+          <Text>Tickets</Text>
           <FlatList
             data={tickets}
             renderItem={({item}) => <TicketCard key={item.id} item={item}/> }
             keyExtractor={(item) => item.id.toString()}
-          />
+            />
           <Button 
             title='Logout'
             onPress={() => navigation.dispatch(
@@ -41,8 +70,9 @@ export default function Home({navigation, route, tickets, loading}) {
                 index: 0,
                 routes: [{ name: 'Sign in' }],
               }))}
-          />
+              />
         <StatusBar style="auto" />
-      </View>
+        {/* </View> */}
+     </ScrollView>
   );
 }
