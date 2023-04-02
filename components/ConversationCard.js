717721c2e9f2a157e145e93424d8e776bc12777d
giabertu <giabertu99@gmail.com>
2023-04-02 16:32:41 +0100

    feat: added conversation and messages state

---
+++ components/ConversationCard.js
@@ -0,0 +1,14 @@
+import {View, Text, Button} from 'react-native'
+
+
+
+
+export default function ConversationCard({item}){
+
+  return (
+    <View>
+      <Text>A convo!</Text>
+    </View>
+  )
+
+}
--- pages/screens/Inbox.js
+++ pages/screens/Inbox.js
@@ -1,26 +1,75 @@
 import {CommonActions} from '@react-navigation/native'
 import React from 'react';
-import { View, StyleSheet, Button, StatusBar } from 'react-native';
+import {useState} from 'react'
+import { View, StyleSheet, Button, StatusBar, FlatList, RefreshControl } from 'react-native';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Text, BottomNavigation } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { styles } from '../../styles';
+// import { styles } from '../../styles';
+import { Searchbar } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import ConversationCard from '../../components/ConversationCard'
 
 
-export default function Inbox({navigation}) {
+export default function Inbox({navigation, user, conversations, messages, loading, fetchData}) {
+  
+  // console.log("Conversations in inbox, ", conversations)
+  const [refreshing, setRefreshing] = useState(false)
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await fetchData();
+    setRefreshing(false);
+  };
+  
   return (
-    <View style={styles.container} >
-        <Text>I wish I had something in my inbox but i dont!!</Text>
-          <Button 
-            title='Logout'
-            onPress={() => navigation.dispatch(
-              CommonActions.reset({
-                index: 0,
-                routes: [{ name: 'Sign in' }],
-              }))}
-          />
+
+    <SafeAreaView  style={styles.container}>
+        <FlatList
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+          renderItem={ConversationCard}
+          data={conversations}
+          keyExtractor={item => item.id}
+        />
         <StatusBar style="auto" />
-      </View>
+    </SafeAreaView>
+
+
+    // <View style={styles.container} >
+    //     <Text>I wish I had something in my inbox but i dont!!</Text>
+    //       <Button 
+    //         title='Logout'
+    //         onPress={() => navigation.dispatch(
+    //           CommonActions.reset({
+    //             index: 0,
+    //             routes: [{ name: 'Sign in' }],
+    //           }))}
+    //       />
+    //     <StatusBar style="auto" />
+    //   </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    marginHorizontal: 16,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+  },
+  header: {
+    fontSize: 32,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+  },
+});
