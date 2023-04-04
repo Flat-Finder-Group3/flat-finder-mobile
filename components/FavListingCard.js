5d8a08834860655aba5378e3e637c42aa1cc3d66
Rohail Ramesh <r.ramesh@se21.qmul.ac.uk>
2023-04-04 11:28:04 +0100

    Dashboard fix

--- components/FavListingCard.js
+++ components/FavListingCard.js
@@ -1,14 +1,24 @@
-import { FlatList, View, Text} from "react-native";
-import { List } from 'react-native-paper'
+import React from "react";
+import { View, Image, ScrollView, Dimensions } from "react-native";
 
-export default function FavListingCard ({item}) {
-
+export default function FavListing({ item }) {
+  const { width } = Dimensions.get("window");
+  const height = (width * 100) / 60;
   return (
-    <View>
-      <Text>{item.title}</Text>
-      <Text>{item.address.city}, {item.address.country}</Text>
-      {/* <Text>{item.status}</Text> */}
+    <View style={{ marginBottom: "90%" }}>
+      <ScrollView
+        pagingEnabled
+        horizontal
+        style={{ width, height, marginTop: "-50%" }}
+      >
+        {item.images.map((image, index) => (
+          <Image
+            key={index}
+            source={{ uri: image }}
+            style={{ width, height, resizeMode: "contain" }}
+          />
+        ))}
+      </ScrollView>
     </View>
-  )
-
+  );
 }
--- pages/screens/Home.js
+++ pages/screens/Home.js
@@ -1,41 +1,52 @@
-import {CommonActions} from '@react-navigation/native'
-import React, { useState } from 'react';
-import { useEffect } from 'react';
-import { View, StyleSheet, Button, StatusBar, FlatList, RefreshControl, ScrollView} from 'react-native';
-
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Text, BottomNavigation } from 'react-native-paper';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-// import { styles } from '../../styles'
-import TicketCard from '../../components/TicketCard';
-import FavListingCard from '../../components/FavListingCard';
-import OwnListingCard from '../../components/OwnListingCard';
+import { CommonActions } from "@react-navigation/native";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import {
-  SafeAreaView,
-  SectionList,
-} from 'react-native';
+  View,
+  StyleSheet,
+  Button,
+  StatusBar,
+  FlatList,
+  RefreshControl,
+  ScrollView,
+} from "react-native";
 
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Text, BottomNavigation } from "react-native-paper";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+// import { styles } from '../../styles'
+import TicketCard from "../../components/TicketCard";
+import FavListingCard from "../../components/FavListingCard";
+import OwnListingCard from "../../components/OwnListingCard";
+import { SafeAreaView, SectionList } from "react-native";
 
-export default function Home({navigation, route, tickets, ownListings, favListings, loading, fetchData}) {
-
+export default function Home({
+  navigation,
+  route,
+  tickets,
+  ownListings,
+  favListings,
+  loading,
+  fetchData,
+}) {
   favListings = favListings.map((item) => item.listing);
 
   const [refreshing, setRefreshing] = useState(false);
 
   const DATA = [
     {
-      title: 'Saved Listings',
-      data: favListings
+      title: "Saved Listings",
+      data: favListings,
     },
     {
-      title: 'Own Listings',
-      data: ownListings
+      title: "Own Listings",
+      data: ownListings,
     },
     {
-      title: 'My Tickets',
-      data: tickets
+      title: "My Tickets",
+      data: tickets,
     },
-  ]
+  ];
 
   const onRefresh = async () => {
     setRefreshing(true);
@@ -43,90 +54,50 @@ export default function Home({navigation, route, tickets, ownListings, favListin
     setRefreshing(false);
   };
 
-
-  if (loading) return (
-    <View>
-      <Text>Loading....</Text>
-    </View>
-  )
+  if (loading)
+    return (
+      <View>
+        <Text>Loading....</Text>
+      </View>
+    );
 
   return (
-
     <SafeAreaView style={styles.container}>
-    <SectionList
-      sections={DATA}
-      keyExtractor={(item, index) => item + index}
-      refreshControl={
-        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-      }
-      renderSectionFooter={({section}) => {
-        if (section.data.length === 0){
-          return <Text>No data D:</Text>
+      <SectionList
+        sections={DATA}
+        keyExtractor={(item, index) => item + index}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
         }
-      }}
-      renderItem={({section, item}) => {
-        // console.log(prop)
-        console.log(section.title, section.data.length)
-        if (section.title === 'Savd Listings'){
-          return <FavListingCard item={item} />
-        } else if (section.title === 'Own Listings'){
-          return <OwnListingCard item={item}/>
-        } else {
-          return <TicketCard item={item}/>
-        }
-      }}
-      renderSectionHeader={({section: {title}}) => (
-        <Text style={styles.header}>{title}</Text>
-      )}
-    />
-  </SafeAreaView>
-
-
-  //   <ScrollView
-  //   contentContainerStyle={styles.container}
-    // refreshControl={
-    //   <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-    // }
-  // >
-
-  //   {/* <View style={styles.container}> */}
-  //       <Text>My empty home!</Text>
-
-  //         <Text>Favorite Listings</Text>
-  //           <FlatList
-  //             data={favListings}
-  //             renderItem={({item}) => <FavListingCard key={item.id} item={item}/> }
-  //             keyExtractor={(item) => item.id.toString()}
-  //             />
-
-
-  //         <Text>Own Listings</Text>
-  //           <FlatList
-  //             data={ownListings}
-  //             renderItem={({item}) => <FavListingCard key={item.id} item={item}/> }
-  //             keyExtractor={(item) => item.id.toString()}
-  //             />
-
-  //         <Text>Tickets</Text>
-  //         <FlatList
-  //           data={tickets}
-  //           renderItem={({item}) => <TicketCard key={item.id} item={item}/> }
-  //           keyExtractor={(item) => item.id.toString()}
-  //           />
-  //         <Button 
-  //           title='Logout'
-  //           onPress={() => navigation.dispatch(
-  //             CommonActions.reset({
-  //               index: 0,
-  //               routes: [{ name: 'Sign in' }],
-  //             }))}
-  //             />
-  //       <StatusBar style="auto" />
-  //       {/* </View> */}
-  //    </ScrollView>
+        renderSectionFooter={({ section }) => {
+          if (section.data.length === 0) {
+            return <Text>No data D:</Text>;
+          }
+        }}
+        renderItem={({ section, item, setTickets, tickets }) => {
+          // console.log(prop)
+          console.log(section.title, section.data.length);
+          if (section.title === "Saved Listings") {
+            return <FavListingCard item={item} />;
+          } else if (section.title === "Own Listings") {
+            return <OwnListingCard item={item} />;
+          } else {
+            return (
+              <TicketCard
+                item={item}
+                setTickets={setTickets}
+                tickets={tickets}
+              />
+            );
+          }
+        }}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.header}>{title}</Text>
+        )}
+      />
+    </SafeAreaView>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -135,13 +106,13 @@ const styles = StyleSheet.create({
     marginHorizontal: 16,
   },
   item: {
-    backgroundColor: '#f9c2ff',
+    backgroundColor: "#f9c2ff",
     padding: 20,
     marginVertical: 8,
   },
   header: {
     fontSize: 32,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   title: {
     fontSize: 24,
